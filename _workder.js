dd8681cd461052602e98d84782649fafb3ef1f97
Vincent He <stariveer@gmail.com>
2023-12-03 19:53:20 +0800

    Update _workder.js

--- _workder.js
+++ _workder.js
@@ -1,58 +1,23 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+const TELEGRAPH_URL = 'https://api.openai.com';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-  const fetchAPI = request.url.replace(url.host, 'api.openai.com');
-  
-  // 部分代理工具，请求由浏览器发起，跨域请求时会先发送一个 preflight 进行检查，也就是 OPTIONS 请求
-  // 需要响应该请求，否则后续的 POST 会失败
-  const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS',
-    'Access-Control-Allow-Headers': '*',
-  };
-  if (request.method === 'OPTIONS') return new Response(null, { headers: corsHeaders });
-
-  const authKey = request.headers.get('Authorization');
-  if (!authKey) return new Response("Not allowed", { status: 403 });
-
-  let contentType = request.headers.get('Content-Type')
-  if (contentType && contentType.startsWith("multipart/form-data")) {
-    let newRequest = new Request(fetchAPI, request);
-    return await fetch(newRequest);
-  }
-
-  let body;
-  if (request.method === 'POST') body = await request.json();
-
-  const payload = {
+  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+  const modifiedRequest = new Request(url.toString(), {
+    headers: request.headers,
     method: request.method,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authKey,
-    },
-    body: typeof body === 'object' ? JSON.stringify(body) : '{}',
-  };
-  // 在 Cloudflare 中，HEAD 和 GET 请求带 body 会报错
-  if (['HEAD', 'GET'].includes(request.method)) delete payload.body;
-
-  // 入参中如果包含了 stream=true，则表现形式为流式输出
-  const response = await fetch(fetchAPI, payload);
-  if (body && body.stream !== true) {
-    const results = await response.json();
-    return new Response(JSON.stringify(results), {
-      status: response.status,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } else {
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: response.headers,
-    });
-  }
+    body: request.body,
+    redirect: 'follow'
+  });
+  const response = await fetch(modifiedRequest);
+  const modifiedResponse = new Response(response.body, response);
+  // 添加允许跨域访问的响应头
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+  return modifiedResponse;
 }
+
